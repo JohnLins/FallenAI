bdb799972b73fa9bb4ca24f9cbfb8c8a3cf7b51c
John Lins <46113320+JohnLins@users.noreply.github.com>
2020-11-28 11:41:04 -0800

    Update model.js

--- model/model.js
+++ model/model.js
@@ -1,6 +1,28 @@
 var email;
 var name;
 
-function fallen(){
-//Call API
+function inputData() {
+    email = document.getElementById("emailText").value;
+    document.getElementById("email").innerHTML = email;
+
+    name = document.getElementById("nameText").value;
+    document.getElementById("name").innerHTML = name;
+  }
+
+
+function fallen() {
+  
+  var message = `${encodeURIComponent(name)} has fallen, please call 911. View Camera: http://localhost:8080/camera.html`
+  var url = `http://localhost:8080/sendEmail?email=${encodeURIComponent(email)}&msg=${encodeURIComponent(message)}`
+
+  var request = new XMLHttpRequest()
+  request.open("GET", url)
+  request.onload = () => {
+      var response = JSON.parse(request.responseText) 
+      document.querySelector("#sentOutput").querySelector("span").innerText = response
+
+  }
+  
+  request.send()
+    
 }
